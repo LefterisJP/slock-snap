de5364f114931980ea93729d2f07bdc46e80123d
Lefteris Karapetsas <lefteris@refu.co>
2016-02-02 18:38:09 +0100

    Integrate ethereum framework

---
+++ bin/nodebinwrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+platform=`uname -i`
+NODE_EXEC=
+
+# choose the right node executable depending on platform
+case $platform in
+  x86_64)
+    NODE_EXEC=node
+    ;;
+  armv7l)
+    NODE_EXEC=arm-node
+    ;;
+  *)
+    echo "unknown platform "
+    ;;
+esac
+
+export NODE_PATH=./lib/node_modules:$NODE_PATH
+$NODE_EXEC ./lib/node_modules/slock/bin/slock-snap.js

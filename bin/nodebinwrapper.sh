63b8a8344cf0d1d5d0e276f4ff6f17e594782c6d
Lefteris Karapetsas <lefteris@refu.co>
2016-02-14 16:56:13 +0100

    Enable yaml config files

--- bin/nodebinwrapper.sh
+++ bin/nodebinwrapper.sh
@@ -16,5 +16,11 @@ case $platform in
     ;;
 esac
 
+# if the config.yaml is not copied to the APP_DATA path copy it there
+if [ ! -e "$SNAP_APP_DATA_PATH/conf/config.yaml" ]; then
+    mkdir -p "$SNAP_APP_DATA_PATH/conf/"
+    cp "$SNAP_APP_PATH/conf/config.yaml" "$SNAP_APP_DATA_PATH/conf/config.yaml"
+fi
+
 export NODE_PATH=./lib/node_modules:$NODE_PATH
-$NODE_EXEC ./lib/node_modules/slock/bin/slock-snap.js
+$NODE_EXEC ./lib/node_modules/slock/bin/slock.js "$SNAP_APP_DATA_PATH/conf/config.yaml"
